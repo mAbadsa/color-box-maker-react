7f861e453e317c65b66e3c6a18e6354c0d28fc36
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-02-05 17:41:05 +0200

    Revert "Uploade Porject Files"
    This reverts commit 152b21b99f893558d73296ae23af3eaa41d52b46, reversing
    changes made to b53701d9ed007ce90463e12f3fdcc52231e0dae6.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import BoxList from "./BoxList";
 import "./App.css";
@@ -11,20 +10,6 @@ class App extends Component {
       </div>
     );
   }
-=======
-import React from 'react';
-import TodoList from './TodoList';
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <TodoList />
-      
-    </div>
-  );
->>>>>>> 8fd1fbb227db95bffbba79c721e3dbc12b744582
 }
 
 export default App;
